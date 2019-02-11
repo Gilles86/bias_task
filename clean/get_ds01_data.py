c90809d32c3aab4a6917a9c8435b9fc6ce6393a6
Gilles86 <Gilles.de.Hollander@gmail.com>
2019-02-11 16:15:09 +0100

    Various tweaks

--- clean/get_ds01_data.py
+++ clean/get_ds01_data.py
@@ -9,28 +9,28 @@ import json
 def main(mode='anat'):
     subjects = ['{:02d}'.format(s) for s in range(1,20)]
 
-    subjects.pop(2)
-    subjects.pop(2)
+    #subjects.pop(2)
+    #subjects.pop(2)
 
-    mapping = {'01':'KCAT',
-               '02':'PF5T',
-               '03':'WW2T',
-               '04':'WSFT',
-               '05':'KP6T',
-               '06':'LV2T',
-               '07':'FMFT',
-               '08':'HCBT',
-               '09':'RSIT',
-               '10':'TS6T',
-               '11':'UM2T',
-               '12':'BI3T',
-               '13':'MRCT',
-               '14':'NM3T',
-               '15':'SC1T',
-               '16':'SPGT',
-               '17':'ZK4T',
-               '18':'DA9T',
-               '19':'VL1T'}
+    mapping = {'01': 'BI3T',
+                    '02': 'DA9T',
+                    '03': 'FMFT',
+                    '04': 'GAIT',
+                    '05': 'HCBT',
+                    '06': 'KCAT',
+                    '07': 'KP6T',
+                    '08': 'LV2T',
+                    '09': 'MRCT',
+                    '10': 'NM3T',
+                    '11': 'PF5T',
+                    '12': 'RSIT',
+                    '13': 'SPGT',
+                    '14': 'TS6T',
+                    '15': 'UM2T',
+                    '16': 'VL1T',
+                    '17': 'WSFT',
+                    '18': 'WW2T',
+                    '19': 'ZK4T'}
 
 
     if mode == 'anat':
@@ -50,7 +50,7 @@ def main(mode='anat'):
             shutil.copy(inv1, os.path.join(new_dir, 'sub-{subject}_inv-1_part-mag_MP2RAGE.nii'.format(**locals())))
 
             inv2 = os.path.join(dir, 'INV2.nii'.format(**locals()))
-            shutil.copy(inv1, os.path.join(new_dir, 'sub-{subject}_inv-2_part-mag_MP2RAGE.nii'.format(**locals())))
+            shutil.copy(inv2, os.path.join(new_dir, 'sub-{subject}_inv-2_part-mag_MP2RAGE.nii'.format(**locals())))
 
             t1w = os.path.join(dir, 'UNI_Images.nii')
             shutil.copy(t1w, os.path.join(new_dir, 'sub-{subject}_T1UNI.nii'.format(**locals())))
@@ -60,6 +60,8 @@ def main(mode='anat'):
 
             for ix, te in enumerate([11.22, 20.39, 29.57]):
 
+                echo = ix+1
+
                 old_fn = os.path.join('/home/raw_data/2018/subcortex/bias_task/raw/ds-01/anat/FLASH', 'sub-{subject}_FLASH_echo_{te}.nii.gz'.format(**locals()))
                 print(old_fn, os.path.exists(old_fn))
 
@@ -67,53 +69,20 @@ def main(mode='anat'):
                 shutil.copy(old_fn, new_fn)
 
     elif mode == 'func':
-        dir = '/home/raw_data/2018/subcortex/bias_task/raw/ds-02/func'
+        dir = '/home/raw_data/2018/subcortex/bias_task/raw/ds-01/func'
 
         for subject in subjects:
 
-            for modality in ['func', 'fmap']:
-                new_dir = os.path.join('/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-02/sub-{subject}/{modality}'.format(**locals()))
-                if not os.path.exists(new_dir):
-                    os.makedirs(new_dir)
-
-            subject_ = mapping[subject]
-            template = os.path.join(dir, 'STNREP_S{subject}*_B0_*.nii'.format(**locals()))
-
-            # B0 MAP
-            B0 = glob.glob(template)
-            if len(B0) == 2:
-                B0 = [b0 for b0 in B0 if 'shimmed' in b0 ]
-
-            if len(B0) != 1:
-                raise Exception('Problem with B0')
-
-            B0 = B0[0]
-            magnitude = image.index_img(B0, 0)
-            phase = image.index_img(B0, 1)
-
-            magnitude.to_filename('/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-02/sub-{subject}/fmap/sub-{subject}_magnitude1.nii'.format(**locals()))
-            phase.to_filename('/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-02/sub-{subject}/fmap/sub-{subject}_phasediff.nii'.format(**locals()))
-
-
             # FUNCTIONAL
-            template = os.path.join(dir, 'STNREP_S{subject}*_TE14_*.nii'.format(**locals()))
-            func = glob.glob(template)
-            func = natsorted(func)
-           
-            fmap_meta = {"EchoTime1": 0.0,
-                         "EchoTime2": 0.00438,
+            fmap_meta = {"EchoTime1": 0.00600,
+                         "EchoTime2": 0.00702,
                          "IntendedFor":[]}
                          
-            for ix, f in enumerate(func):
-                run = '{:02d}'.format(ix+1)
-                template = "func/sub-{subject}_task-randomdotmotion_run-{run}_bold.nii.gz".format(**locals())
+            for run in [1,2,3]:
+                fmap_meta['IntendedFor'].append('func/sub-{subject}_task-randomdotmotion_run-{run:02d}_bold.nii'.format(**locals()))
 
-                shutil.copy(f, os.path.join('/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-02/', 
-                                            'sub-{subject}', template).format(**locals()))
 
-                fmap_meta['IntendedFor'].append(template)
-            
-            json_file = '/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-02/sub-{subject}/fmap/sub-{subject}_phasediff.json'.format(**locals())
+            json_file = '/home/raw_data/2018/subcortex/bias_task/sourcedata/ds-01/sub-{subject}/fmap/sub-{subject}_phasediff.json'.format(**locals())
 
             with open(json_file, 'w') as outfile:
                 json.dump(fmap_meta, outfile)
